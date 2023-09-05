2cd206c11dfe088bb1207e4755da390b7a89a516
darinetag <tagdarinefatma@gmail.com>
2023-09-05 21:35:17 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+ const wantedNumbers = {};
+
+  
+  for (let num of array) {
+    
+    const complement = target - num;
+
+   
+    if (wantedNumbers[complement]) {
+      return true;
+    }
+
+    
+    wantedNumbers[num] = true;
+} 
+return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); 
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));   
+console.log(hasTargetSum([1, 2, 5], 4));             
 
 /* 
-  Add your pseudocode here
+  Big O time complexity of function here
+  O(n) linear time complexity algorithm, because it iterates over the array once, with constant number of times making it linear gradually
+*/
+
+/* Pseudocode:
+1. Create an empty object called `wantedNumbers` to keep track of numbers we've seen.
+2. For each number `num` in the `array`:
+   a. Calculate the `complement` as `target - num`.
+   b. Check if `complement` exists in `wantedNumbers`.
+   c. If found, return true (a pair with the sum `target` exists).
+   d. Otherwise, mark `num` as "wanted" by adding it as a key in `wantedNumbers`.
+3. If no pairs are found after iterating through the array, return false.
+
+ 
 */
 
 /*
-  Add written explanation of your solution here
+  initialized the wantedNumbers fnc to keep track of the wanted numbers
+ iterate over the array for each number
+ -calculate the complemenet ( num + num = wanted number)
+ - if found return true
+ - if not found after iteration return false 
 */
 
 // You can run `node index.js` to view these console logs
